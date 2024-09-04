3fecbfa44ecc5566cc33590b290fb254c50d3325
Maria <167232353+MahhPaiva@users.noreply.github.com>
2024-09-04 12:21:43 -0300

    Update Questao3.java
    Correção de um erro na lógica

--- Questao3.java
+++ Questao3.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Questao3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double faturamento[] = new double[5];
+        double faturamento[] = new double[31];
         double media=0, maiorValor=faturamento[1], menorValor=100000, cont=0, diasSupMedia=0;
         
-        for(int i=1; i<5; i++){
+        for(int i=1; i<=30; i++){
             System.out.print("Informe o faturamento do dia " + i + ": ");
             faturamento[i] = Double.parseDouble(scanner.nextLine());
             if(faturamento[i]>maiorValor){
